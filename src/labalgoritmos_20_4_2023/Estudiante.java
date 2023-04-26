ea5d9e95d8d129909cdbeb92db2a34b72c0e062d
luisballar <luisballar123@gmail.com>
2023-04-25 16:49:23 -1100

    Ajuste

--- src/labalgoritmos_20_4_2023/Estudiante.java
+++ src/labalgoritmos_20_4_2023/Estudiante.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package labalgoritmos_20_4_2023;
 
-/**
- *
- * @author LabB
- */
 public class Estudiante {
-    
+
     private String nombre;
     private String ID;
     private String telefono;
@@ -64,9 +55,5 @@ public class Estudiante {
     public void setEdad(int edad) {
         this.edad = edad;
     }
-
-    
-        
-    
     
 }
--- src/labalgoritmos_20_4_2023/Metodos.java
+++ src/labalgoritmos_20_4_2023/Metodos.java
@@ -1,27 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package labalgoritmos_20_4_2023;
 
-/**
- *
- * @author LabB
- */
 public class Metodos extends Estudiante{
-    
+
     public Metodos(String nombre, String ID, String telefono, char sexo, int edad) {
         super(nombre, ID, telefono, sexo, edad);
     }
-    
-    
-   public StringBuilder menuImprimir(){
-       StringBuilder sB = new
-       
-       
-   }
-    
-    
-    
+
+
+
 }
+
