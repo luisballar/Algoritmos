abc52a0ca4a48112edfa54dbdd8977023c8889cd
jesusCS2000 <castillosalazarjesus30@gmail.com>
2023-04-26 09:50:52 -0600

    nuevos cambios(ya hice la opcion 4, mostrar todos los datos del estudiante, con datos pasados por refencia)

--- src/labalgoritmos_20_4_2023/Metodos.java
+++ src/labalgoritmos_20_4_2023/Metodos.java
@@ -35,56 +35,68 @@ public class Metodos //extends Estudiante
    public void entradaOpcion(){
        int opcion =0;
        Scanner entrada = new Scanner(System.in);
-       while(opcion<=0 || opcion >11){
+       while(opcion<=0 || opcion >11) {
            System.out.println("Ingrese la opción");
            opcion = entrada.nextInt();
-       }
-       switch(opcion){
-           case 1:
-               System.out.println("1");
-               listaEnlazada Estudiantes = new listaEnlazada();
-               Estudiantes.agregarPruebas("Jesús;C21845;6333 8045; M; 20");
-               Estudiantes.agregarPruebas("Pedro;C01235;6933 8142; M; 19");
-               Estudiantes.agregarPruebas("Andrea;C23485;6003 8489; F; 20");
-               break;
-           case 2:
-               System.out.println("2");
-               break;
-           case 3:
-               System.out.println("3");
-               break;
-           case 4:
-               System.out.println("Todos los datos");
-              // Nodo head = new Nodo("Jesús;C21845;6333 8045; M; 20", Estudiante2);
-              // Nodo Estudiante2 = new Nodo("Pedro;C01235;6933 8142; M; 19",Estudiante3);
-              // Nodo Estudiante3 = new Nodo("Andrea;C23485;6003 8489; F; 20", null);
-               listaEnlazada EstudiantesTodosLosDatos = new listaEnlazada();
-               EstudiantesTodosLosDatos.agregarPruebas("Jesús;C21845;6333 8045; M; 20");
-               EstudiantesTodosLosDatos.agregarPruebas("Pedro;C01235;6933 8142; M; 19");
-               EstudiantesTodosLosDatos.agregarPruebas("Andrea;C23485;6003 8489; F; 20");
-               EstudiantesTodosLosDatos.recorrerLista();
-
-               break;
-           case 5:
-               System.out.println("5");
-               break;
-           case 6:
-               System.out.println("6");
-               break;
-           case 7:
-               System.out.println("7");
-               break;
-           case 8:
-               System.out.println("8");
-               break;
-           case 9:
-               System.out.println("9");
-               break;
-           case 10:
-               System.out.println("10");
-               break;
-           case 11:
-               break;
+           if(opcion>0 || opcion <=11){
+               if(opcion != 11) {
+                   switch (opcion) {
+                       case 1:
+                           System.out.println("1");
+                           listaEnlazada Estudiantes = new listaEnlazada();
+                           Estudiantes.agregarPruebas("Jesús;C21845;6333 8045; M; 20");
+                           Estudiantes.agregarPruebas("Pedro;C01235;6933 8142; M; 19");
+                           Estudiantes.agregarPruebas("Andrea;C23485;6003 8489; F; 20");
+                           opcion=0;
+                           break;
+                       case 2:
+                           System.out.println("2");
+                           opcion=0;
+                           break;
+                       case 3:
+                           System.out.println("3");
+                           opcion=0;
+                           break;
+                       case 4:
+                           System.out.println("Todos los datos");
+                           // Nodo head = new Nodo("Jesús;C21845;6333 8045; M; 20", Estudiante2);
+                           // Nodo Estudiante2 = new Nodo("Pedro;C01235;6933 8142; M; 19",Estudiante3);
+                           // Nodo Estudiante3 = new Nodo("Andrea;C23485;6003 8489; F; 20", null);
+                           listaEnlazada EstudiantesTodosLosDatos = new listaEnlazada();
+                           EstudiantesTodosLosDatos.agregarPruebas("Jesús;C21845;6333 8045; M; 20");
+                           EstudiantesTodosLosDatos.agregarPruebas("Pedro;C01235;6933 8142; M; 19");
+                           EstudiantesTodosLosDatos.agregarPruebas("Andrea;C23485;6003 8489; F; 20");
+                           EstudiantesTodosLosDatos.recorrerLista();
+                           opcion=0;
+                           break;
+                       case 5:
+                           System.out.println("5");
+                           opcion=0;
+                           break;
+                       case 6:
+                           System.out.println("6");
+                           opcion=0;
+                           break;
+                       case 7:
+                           System.out.println("7");
+                           opcion=0;
+                           break;
+                       case 8:
+                           System.out.println("8");
+                           opcion=0;
+                           break;
+                       case 9:
+                           System.out.println("9");
+                           opcion=0;
+                           break;
+                       case 10:
+                           System.out.println("10");
+                           opcion=0;
+                           break;
+                   }
+               }else
+                   opcion=11;
+           }
        }
    }
 
